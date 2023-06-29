5bbec493af6fe0274de2704ef5142736a28f6d41
Olza <olza99998@gmail.com>
2023-06-29 13:13:57 +0300

    Fixes author page, added Policy page

--- js/main.js
+++ js/main.js
@@ -144,18 +144,19 @@ document.addEventListener('DOMContentLoaded', function () {
   // Accordion
   if (document.querySelector('.accordion')) {
     // (Optional) Active an item if it has the class "is-active"  
-    document.querySelector('.accordion-content .accordion-item.active');
+    // document.querySelector('.accordion-content .accordion-item.active');
 
     document.querySelectorAll('.accordion-content .accordion-item > .accordion-heading').forEach((item) => {
       item.addEventListener('click', function () {
         //event.preventDefault();
+        let accordion = item.closest('.accordion');
         let content = item.parentNode.querySelector('.accordion-text');
         // Cancel the siblings
         if (item.parentNode.classList.contains('active')) {
           item.parentNode.classList.remove('active')
           content.style.maxHeight = 0;
         } else {
-          if (document.querySelector('.accordion-content .accordion-item.active')) {
+          if (!accordion.classList.contains('accordion-multiple') && document.querySelector('.accordion-content .accordion-item.active')) {
             document.querySelector('.accordion-content .accordion-item.active .accordion-text').style.maxHeight = 0;
             document.querySelector('.accordion-content .accordion-item.active').classList.remove('active');
           }
